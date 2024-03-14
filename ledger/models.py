03bdc8bc15c149281958f4079e097b6e04e34ed4
NxKuma <neilisraelko01@gmail.com>
2024-03-14 21:06:48 +0800

    Modified Recipe Model and HTML

--- ledger/models.py
+++ ledger/models.py
@@ -11,6 +11,9 @@ class Profile(models.Model):
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=50)
+    author = models.CharField(max_length=50)
+    created_on = models.DateTimeField(auto_now_add=True) 
+    updated_on = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
